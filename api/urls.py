b7722335c0cb9a96e30a67fbcb698cfbe5b18f50
NoDuckyAnyMore <1026383879@qq.com>
2025-06-19 22:07:48 +0800

    create repo

---
+++ api/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from . import views
+from rest_framework.documentation import include_docs_urls
+
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version='v1',
+        description="这是你的接口文档",
+        terms_of_service="https://www.example.com",
+        contact=openapi.Contact(email="your@email.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+ 
+
+urlpatterns = [
+    path('helloWorld/', views.get_helloWorld, name='hello_world'),
+    path('goods/', views.goods_list, name='get_data'),
+    path('goods/<int:id>/', views.goods_detail, name='goods_detail'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # 可选添加 Redoc 风格文档：
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]  
